632251e7f1502426a93847f4e0b7d55e99f73c14
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-26 20:38:46 +0100

    pushati comics nel db

--- database/migrations/2024_01_25_182721_create_comics.php
+++ database/migrations/2024_01_25_182721_create_comics.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string("title");
-            $table->string("description");
+            $table->text("description");
             $table->string("thumb");
-            $table->float("price", 4, 2);
+            $table->string("price", 6);
             $table->string("series");
             $table->date("sale_date");
             $table->string("type");
