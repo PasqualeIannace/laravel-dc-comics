a7e8e21377cd1586f096852100a669164100b15b
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-26 20:17:37 +0100

    Creato controller con resource + seeder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index'])->name("home");
+Route::get('/', [ComicController::class, 'index'])->name("home");
+
+Route::resource("comics", ComicController::class);
