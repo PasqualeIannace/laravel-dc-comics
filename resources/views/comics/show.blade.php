6adbdc704fded0c6b030428244ee4346fdcc32a0
PasqualeIannace <pasquale.iannace@gmail.com>
2024-01-29 22:00:42 +0100

    add: delete

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -15,7 +15,13 @@
             <p>{{ $fumetto->description }}</p>
 
             <div>
-                <a href="{{ route('comics.edit', $fumetto->id) }}" type="submit" class="btn btn-warning">Modifica</a>
+                <a href="{{ route('comics.edit', $fumetto->id) }}" class="btn btn-warning">Modifica</a>
+
+                <form action="{{ route('comics.destroy', $fumetto->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Elimina" class="btn btn-danger">
+                </form>
             </div>
         </div>
         <div class="col-3">
